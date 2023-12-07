cb74829ef6b68c44e32c009097edbece77bc6174
Myroslava <tarcham1993@gmail.com>
2023-12-07 17:22:48 +0100

    finish car price prediction task

--- LinearRegression/LinReg_gradient_descent.py
+++ LinearRegression/LinReg_gradient_descent.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 class LinearRegressionGD:
-    def __init__(self, l=0.001, iter=1000):
+    def __init__(self, l=0.001, iter=1000, penalty=1):
         self.l = l
         self.n_i = iter
+        self.penalty = penalty
         self.weights = None
         self.bias = None
 
     def fit(self, X, y):
         n_s, n_f = X.shape
+        self.m = n_s
+        self.n = n_f
 
         self.bias = 0
         self.weights = np.zeros(n_f)
@@ -16,7 +19,12 @@ class LinearRegressionGD:
         for i in range(self.n_i):
             y_pred = self.predict(X)
 
-            dw = (1 / n_s) * np.dot(X.T, (y_pred - y))
+            dw = (1 / n_s) * (np.dot(X.T, (y_pred - y)))
+
+            # regularization did not help much so commented out. but maybe because there was no overfit
+            #dw = (1 / n_s) * (np.dot(X.T, (y_pred - y)) + self.penalty * self.weights) <- ridge reg
+            #dw = self.__apply_lasso_regularization(dw) <- lasso
+            
             db = (1 / n_s) * np.sum(y_pred - y)
 
             self.weights -= self.l * dw
@@ -24,3 +32,12 @@ class LinearRegressionGD:
 
     def predict(self, X):
         return self.bias + np.dot(X, self.weights)
+    
+    def __apply_lasso_regularization(self, dw):
+        for j in range( self.n ) : 
+            if self.weights[j] > 0 : 
+                dw[j] -= self.penalty / self.m 
+            else :  
+                dw[j] += self.penalty / self.m 
+
+        return dw
--- LinearRegression/LinReg_normal_equation.py
+++ LinearRegression/LinReg_normal_equation.py
@@ -1,18 +1,22 @@
 import numpy as np
 
 class LinearRegressionNE:
-    def __init__(self):
+    def __init__(self, regularization=0.1):
         self.weights = None
+        self.r = regularization
 
     def fit(self, X, y):
         ones = np.ones(X.shape[0])
         X_1 = np.column_stack([ones, X])
 
         XTX = X_1.T.dot(X_1)
+
+        # to avoid singular matrix problem - add small number to diagonal
+        XTX = XTX + self.r * np.eye(XTX.shape[0])
+
         XTX_inv = np.linalg.inv(XTX)
         w = XTX_inv.dot(X_1.T).dot(y)
 
-        # (XT*X)^-1*XT*y
         self.weights = w
 
     def predict(self, X):
