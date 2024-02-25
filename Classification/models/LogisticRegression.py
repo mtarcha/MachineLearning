e00bd87f231e2c1d8655034720cfe0d0b2b13257
Myroslava <tarcham1993@gmail.com>
2024-02-25 21:08:12 +0100

    logistic regression model

---
+++ Classification/models/LogisticRegression.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class LogisticRegression:
+    """
+    My implementation from scratch of LR
+    """
+
+    def __init__(self, lr=0.001, iterations=1000):
+        self.lt = lr
+        self.iterations = iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        m_s, n_f = X.shape
+        self.weights = np.zeros(n_f)
+        self.bias = 0
+
+        # gradient descent
+        for i in range(self.iterations):
+            y_pred = self._predict(X)
+            dw = np.dot(X.T, (y_pred - y)) / m_s
+            db = np.sum(y_pred - y) / m_s
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        y_pred = self._predict(X)
+        y_pred_cls = [1 if i > 0.5 else 0 for i in y_pred]
+        return y_pred_cls
+    
+    
+    def _predict(self, X):
+        lin_model = np.dot(X, self.weights) + self.bias
+        y_pred = self._sigmoid(lin_model)
+        return y_pred
+
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
